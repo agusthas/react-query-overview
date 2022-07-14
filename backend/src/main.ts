f5ae11885fd565831c4d1d60ffb8031f3515b23e
SusuMantan <suzumantan@gmail.com>
2022-07-15 02:41:23 +0700

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  // Swagger
+  const documentConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('api', app, document);
 
   // CORS
   app.enableCors();
