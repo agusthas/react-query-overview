b1c9fd0dcff228eacdd82f9c720a40ede2b27531
SusuMantan <suzumantan@gmail.com>
2022-07-15 11:41:55 +0700

    add colorscheme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,42 @@
 import {
   Anchor,
+  ColorScheme,
+  ColorSchemeProvider,
   Container,
   Divider,
   Group,
   MantineProvider,
+  SegmentedControl,
   Title,
+  useMantineColorScheme,
 } from '@mantine/core';
+import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
+import queryClient from './config/queryClient';
 import NotFoundPage from './pages/404';
 import Home from './pages/Home';
-import queryClient from './config/queryClient';
 import ProductPage from './pages/Products';
 
-function App() {
+function AppLayout() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   return (
     <Container py="lg">
       <Title align="center" mb="md">
         React Query Overview
       </Title>
+
+      <Group position="center" mb="md">
+        <SegmentedControl
+          value={colorScheme}
+          onChange={() => toggleColorScheme()}
+          data={[
+            { value: 'light', label: 'Light' },
+            { value: 'dark', label: 'Dark' },
+          ]}
+        />
+      </Group>
 
       <Group position="center">
         <Anchor component={Link} to="/">
@@ -37,27 +54,44 @@ function App() {
   );
 }
 
-function AppWrapper() {
+function App() {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <MantineProvider
-        withNormalizeCSS
-        withGlobalStyles
-        theme={{ colorScheme: 'dark' }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route index element={<Home />} />
-              <Route path="products" element={<ProductPage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
-      </MantineProvider>
+        <MantineProvider
+          withNormalizeCSS
+          withGlobalStyles
+          theme={{ colorScheme }}
+        >
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<AppLayout />}>
+                <Route index element={<Home />} />
+                <Route path="products" element={<ProductPage />} />
+              </Route>
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+        </MantineProvider>
+      </ColorSchemeProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
 
-export default AppWrapper;
+export default App;
